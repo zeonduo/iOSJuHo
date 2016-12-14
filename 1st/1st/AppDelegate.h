bbaeb9c628418d6186b9ca108d05ca19c98d95d3
Juho Yoon <juhoyoon@Juhoui-Device.local>
2016-12-14 18:29:29 +0900

    1st 커밋

---
+++ 1st/1st/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1st
+//
+//  Created by Juho Yoon on 2016. 12. 14..
+//  Copyright © 2016년 Juho Yoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1st/1st/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1st
+//
+//  Created by Juho Yoon on 2016. 12. 14..
+//  Copyright © 2016년 Juho Yoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
